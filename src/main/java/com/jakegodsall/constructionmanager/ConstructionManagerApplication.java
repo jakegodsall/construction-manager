6267ccdd90c4059cc235fe38a0f7efbf94c707c9
jakegodsall <jake.edward.godsall@gmail.com>
2023-10-04 11:56:37 +0200

    initial commit

---
+++ src/main/java/com/jakegodsall/constructionmanager/ConstructionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.constructionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstructionManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jakegodsall/constructionmanager/ConstructionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.constructionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
