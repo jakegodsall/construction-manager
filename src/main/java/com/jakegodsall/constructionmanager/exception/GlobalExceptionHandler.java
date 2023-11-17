d35924141d1c71f7692c396b46a16dc5d19b5a4d
jakegodsall <jake.edward.godsall@gmail.com>
2023-11-17 19:58:01 +0100

    added global exception handler for ResourceNotFound exception and bean validation

---
+++ src/main/java/com/jakegodsall/constructionmanager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.jakegodsall.constructionmanager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Map<String, String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        List<Map<String, String>> errorList = methodArgumentNotValidException.getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(errorList);
+
+    }
+
+}
