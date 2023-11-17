283ed2c5efe6a4acd8aeb07237a5338e0d5b984d
jakegodsall <jake.edward.godsall@gmail.com>
2023-11-17 22:10:56 +0100

    refactored exception to use lombok

--- src/main/java/com/jakegodsall/constructionmanager/exception/ResourceNotFoundException.java
+++ src/main/java/com/jakegodsall/constructionmanager/exception/ResourceNotFoundException.java
@@ -1,9 +1,13 @@
 package com.jakegodsall.constructionmanager.exception;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
+@Getter @Setter
 public class ResourceNotFoundException extends RuntimeException {
     private String resourceName;
     private String fieldName;
@@ -13,30 +17,6 @@ public class ResourceNotFoundException extends RuntimeException {
         super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
-    }
-
-    public String getResourceName() {
-        return resourceName;
-    }
-
-    public void setResourceName(String resourceName) {
-        this.resourceName = resourceName;
-    }
-
-    public String getFieldName() {
-        return fieldName;
-    }
-
-    public void setFieldName(String fieldName) {
-        this.fieldName = fieldName;
-    }
-
-    public Long getFieldValue() {
-        return fieldValue;
-    }
-
-    public void setFieldValue(Long fieldValue) {
         this.fieldValue = fieldValue;
     }
 }
